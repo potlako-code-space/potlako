01aa4fb4727d88a050e367203e25a5fa9b1cdf57
nmunatsibw <mcturnergarai@gmail.com>
2023-08-28 20:00:58 +0200

    Update SQLite settings during test in settings.py
    This commit includes changes to settings.py, altering the database configuration during execution of test cases. Instead of the previous configuration that only specified the SQLite engine, the new configuration also defines the name of the SQLite database file. This new setting ensures data consistency and enhances debugging capabilities when running test scenarios.

--- potlako/settings.py
+++ potlako/settings.py
@@ -337,7 +337,7 @@ if 'test' in sys.argv:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
     class DisableMigrations:
